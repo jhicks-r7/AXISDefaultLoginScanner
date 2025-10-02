0f6b1104b2c52c5aabc43f4ec6baa84e85d69486
Joshua Hicks <joshua@jhicks.net>
2025-10-02 13:13:42 -0400

    Committed the wrong version whoops lol

--- axis-login.py
+++ axis-login.py
@@ -2,30 +2,43 @@ import requests
 from requests.auth import HTTPDigestAuth
 import sys
 
-target_file = sys.argv[1]
+requests.packages.urllib3.disable_warnings()
+
+targets_file = sys.argv[1]
 login_endpoints = ['axis-cgi/login.cgi', 'axis-cgi/usergroup.cgi', 'axis-cgi/lightcontrol.cgi']
 username = 'root'
 password = 'pass'
-
+debug_log_file = 'axis-login-debug.log'
 
 def try_login(target): 
     for endpoint in login_endpoints:
-        response=requests.get(f'{target}/{endpoint}', auth=HTTPDigestAuth(username, password), verify=False)
+        if target[-1] == '/':
+            url = f'{target}{endpoint}'
+        else:
+            url = f'{target}/{endpoint}'
+        
+        response=requests.get(f'{target}{endpoint}', auth=HTTPDigestAuth(username, password), verify=False)
         if response.status_code == 200:
             return True
         elif response.status_code == 401:
-            print(response)
             return False
         elif response.status_code == 404:
-            print("endpoint {} not found".format(endpoint))
+            pass
         else:
-            print("Unknown status code: {}".format(response.status_code))
-
-    print("Unable to login to {}".format(target))
+            with open(debug_log_file, 'a+') as f:
+                f.write('-----------\n')
+                f.write("Unknown response when authenticating to {}\n".format(URL))
+                f.write(response.request.headers)
+                f.write('\n')
+                f.write(response.text)
+                f.write('\n')
+            return False
+    print("Login URL could not be found for {}".format(target))
+    print(response.status_code)
     return False
 
 def load_targets():
-    with open(target_file, 'r') as f:
+    with open(targets_file, 'r') as f:
         targets = f.read().splitlines()
     return targets
 
@@ -36,7 +49,7 @@ def main():
         if result==True:
             print("{} is using default creds root/pass".format(t))
         if result==False:
-            print("{} NOT is using default creds root/pass".format(t))
+            print("{} is NOT using default creds root/pass".format(t))
             
 if __name__ == "__main__":
     main()
